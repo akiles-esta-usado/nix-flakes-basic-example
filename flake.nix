a52a495e8cf5a2356d8357712c622b53ff4d9ec2
Aquiles Viza <aquiles.viza@sansano.usm.cl>
2025-08-26 13:10:48 -0400

    Using eachSystem pattern

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
     nixpkgs.follows = "librelane/nix-eda/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { 
+    self,
+    nixpkgs,
+    ... }:
   let
     lib = nixpkgs.lib;
     eachSystem = lib.genAttrs [
@@ -16,10 +19,24 @@
       # "aarch64-darwin"
     ];
   in {
+    packages = eachSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        hello = pkgs.hello;
+        default = self.packages.${system}.hello;
+      }
+    );
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    # packages.x86_64-linux.hello = pkgs.hello;
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
   };
 }
+
+/**
+  legacyPackages
+  - Es un nombre sustituto al de "packages"
+  - `nix flake show` sería horriblemente lento si se reutilizara el mismo nombre
+  - Por defecto, no muestra paquetes listados en "legacyPackages".
+ */
